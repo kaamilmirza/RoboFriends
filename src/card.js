d583f22171e0c468d8ebcf02631b8706b1e51e93
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-06-02 23:25:19 +0530

    final

--- src/card.js
+++ src/card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = () =>{
+const Card = ({name, email, id}) =>{
     return (
-        <div>
-            <img src = '#' alt = 'robots' />
+        <div class = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            
             <div>
-                <h2>Kaamil</h2>
-                <p>kaamilmirza@yahoo.com</p>
+                <img src = {`https://robohash.org/${id}?200x200`} />
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
