66a1568ac3db49ad7b52be1048ddf6b78bf4462f
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-06-04 19:08:16 +0530

    4thchangetoday

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './Cardlist';
 import {robots} from './robot';
 import SearchBox from './SearchBox'
-const App = () =>{
-    return(
-        <div>
+ 
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state ={
+            
+                robots :robots,
+                searchfield: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield : event.target.value})
+       
+        
+    }
+    render(){
+        const filteredRobot = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+       return(
+        <div className = "tc">
         <h1>RoboFriends</h1>
-        <SearchBox/>
-        <CardList robots = {robots} />
+        <SearchBox searchChange = {this.onSearchChange}/>
+        <CardList robots = {filteredRobot} />
         </div>
      );
-
+    }
 }
+
 export default App;
