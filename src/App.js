bb4f078a21faefe9bfcd4534bbe6f76346ef1741
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-06-05 22:41:44 +0530

    lateadd

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ class App extends Component{
     constructor(){
         super();
         this.state ={
-            
                 robots :robots,
                 searchfield: ''
         }
     }
+
     onSearchChange = (event) => {
         this.setState({searchfield : event.target.value})
-       
-        
     }
+
     render(){
         const filteredRobot = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
