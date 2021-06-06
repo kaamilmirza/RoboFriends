8f9b99475886e597be93b445b1b87558f304004d
kaamilmirza <kaamil.mirza.2002@gmail.com>
2021-06-06 12:47:10 +0530

    working

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import CardList from './Cardlist';
-import {robots} from './robot';
-import SearchBox from './SearchBox';
+import CardList from '../components/Cardlist';
+import {robots} from '../robot';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 class App extends Component{
     constructor(){
         super();
